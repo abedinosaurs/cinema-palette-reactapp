439e1afa9e8e364846240db2298ba404b0356fb3
Jason <jason.a.mckenzie@gmail.com>
2020-05-13 20:13:51 -0400

    add format selector, snackbar, footer

--- src/Palette.js
+++ src/Palette.js
@@ -9,26 +9,38 @@ export default class Palette extends Component {
 
 		this.state = {
 			level: 500,
+			format: "hex",
 		};
 		this.changeLevel = this.changeLevel.bind(this);
+		this.changeFormat = this.changeFormat.bind(this);
 	}
 
 	changeLevel(level) {
 		this.setState({ level });
 	}
 
+	changeFormat(val) {
+		this.setState({ format: val });
+	}
+
 	render() {
-		const { colors } = this.props.palette;
-		const { level } = this.state;
+		const { colors, paletteName, emoji } = this.props.palette;
+		const { level, format } = this.state;
 		const colorBoxes = colors[level].map((color) => (
-			<ColorBox background={color.hex} name={color.name} />
+			<ColorBox background={color[format]} name={color.name} key={color.id} />
 		));
 		return (
 			<div className="Palette">
-				{/* Navbar goes here */}
-				<Navbar level={level} changeLevel={this.changeLevel} />
+				<Navbar
+					level={level}
+					changeLevel={this.changeLevel}
+					handleChange={this.changeFormat}
+				/>
 				<div className="Palette-colors">{colorBoxes}</div>
-				{/* footer */}
+				<footer className="Palette-footer">
+					{paletteName}
+					<span className="emjoi">{emoji}</span>
+				</footer>
 			</div>
 		);
 	}
