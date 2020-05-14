7853aaf8c9395a915d6fafa4e203e17c7c06a42e
Jason <jason.a.mckenzie@gmail.com>
2020-05-14 13:40:26 -0400

    link home to palettes through minipalettes

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -34,6 +34,9 @@ const styles = {
 };
 
 class PaletteList extends Component {
+	goToPalette(id) {
+		this.props.history.push(`/palette/${id}`);
+	}
 	render() {
 		const { palettes, classes } = this.props;
 		return (
@@ -44,7 +47,7 @@ class PaletteList extends Component {
 					</nav>
 					<div className={classes.palettes}>
 						{palettes.map((p) => (
-							<MiniPalette {...p} />
+							<MiniPalette {...p} handleClick={() => this.goToPalette(p.id)} />
 						))}
 					</div>
 				</div>
